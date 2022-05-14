bc1897cb4ad68ff732aa3716b4d293b242ee7592
miglenamag <105078615+miglenamag@users.noreply.github.com>
2022-05-14 22:16:43 +0300

    Initial Commit

---
+++ Summator/Summator.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Summator
+{
+    public static class Summator
+    {
+        public static long Sum(int[] arr)
+        {
+            checked
+            {
+                long sum = 0;
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+
+                return sum;
+            }
+        }
+
+        public static int Average(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum / arr.Length;
+        }
+    }
+}
